b7a516f9474becf69b765a6d21167791711241e3
Gabriel Lewertowski <gabriel@redclockwork>
2014-05-06 01:00:25 +0200

    ajoute django_browser

--- habeascorpus/settings.py
+++ habeascorpus/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'django_browser',
 )
 
 MIDDLEWARE_CLASSES = (
--- habeascorpus/urls.py
+++ habeascorpus/urls.py
@@ -1,7 +1,6 @@
-from django.conf.urls import patterns, include, url
-from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls import include, url
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^api/', include('api.urls')),
-)
+    url(r'^browser/', include('django_browser.urls'))
+]
