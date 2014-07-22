96acd644aca391f85b63eec0e02b0273330c8603
Gabriel L <glewerto@gmail.com>
2014-07-22 10:36:59 +0200

    suppression des trucs inutiles pour le serveur

--- django_browser/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-from django_browser import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index')
-]
--- django_browser/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'django_browser/index.html')
